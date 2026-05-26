da9922edd9f7bd561cbf47182a352fb3562a0588
Kambham Harshitha <kambhamharshitha5@gmail.com>
2026-05-26 10:11:10 +0530

    Questions

---
+++ questions.py
@@ -0,0 +1,28 @@
+questions = [
+
+    {
+        "question": "What is Python?",
+        "answer": "python programming language"
+    },
+
+    {
+        "question": "Explain OOPs concepts.",
+        "answer": "class object inheritance polymorphism"
+    },
+
+    {
+        "question": "What is Artificial Intelligence?",
+        "answer": "simulation human intelligence"
+    },
+
+    {
+        "question": "What is Machine Learning?",
+        "answer": "machine learning ai data"
+    },
+
+    {
+        "question": "Difference between SQL and NoSQL?",
+        "answer": "sql structured nosql unstructured"
+    }
+
+]
